0abc8d3088b89fc3fd72a52df928983b4da29271
Pasi Ahopelto <pahopelt@Pasis-MacBook-Pro.local>
2016-03-15 22:08:53 +0200

    More cleanup
    Replaced main.js with search.js and main.html with search.html as
    a preparation to search view and controller.

---
+++ app/scripts/controllers/search.js
@@ -0,0 +1,5 @@
+/*global angular*/
+angular.module('figViewApp')
+    .controller('SearchCtrl', function () {
+        'use strict';
+    });
--- test/spec/controllers/search.js
+++ test/spec/controllers/search.js
@@ -5,19 +5,17 @@ describe('Controller: MainCtrl', function () {
   // load the controller's module
   beforeEach(module('figViewApp'));
 
-  var MainCtrl,
+  var SearchCtrl,
     scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
+    SearchCtrl = $controller('SearchCtrl', {
       $scope: scope
-      // place here mocked dependencies
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(MainCtrl.awesomeThings.length).toBe(3);
+  it('space holder', function () {
   });
 });
